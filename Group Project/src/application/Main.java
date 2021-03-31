380103946a8dfc58332060c6842d9b5fe5dba0ae
Courtney Evins <koinuevins@gmail.com>
2021-03-30 19:49:53 -0500

    edited main.java with comments and scene correction
    Due to git issues, re-cloned the project and made edits to push. This
    will be a test to ensure it's working properly.

--- Group Project/src/application/Main.java
+++ Group Project/src/application/Main.java
@@ -1,3 +1,12 @@
+/*
+ * Group 11: Couch Potatoes
+ * Class: CS3443-003
+ * Assignment: Group project, Title: Pick of the Day
+ * Main.java
+ * 
+ * Main sets the stage and Scene1.fxml scene
+ */
+
 package application;
 	
 import javafx.application.Application;
@@ -11,7 +20,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Scene4.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Scene1.fxml"));
 			Scene scene = new Scene(root,600,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
